a89c26d051a527b2026451d2ca46aedf7ff61e86
kkpatel05 <pkrish501@gmail.com>
2025-07-16 20:01:16 -0400

    Removed test-recipies.html and .js

--- frontend/test-recipes.js
+++
@@ -1,9 +0,0 @@
-import { getRecipes } from "./firebase-config.js";
-
-getRecipes()
-  .then(response => {
-    console.log("Recipes:", response.data); // This logs the recipes to the browser console
-  })
-  .catch(err => {
-    console.error("Error fetching recipes:", err);
-  });
