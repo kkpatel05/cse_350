aaf423c63fd091d44e68d41b8eb4589f015123a9
Aiden Schrenger <aschre2005@gmail.com>
2025-07-16 20:54:28 -0400

    login implementation

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFunctions, httpsCallable } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoNcr7QGFQ_iOXpY0MzFBsdOSP06cBc-0",
-  authDomain: "grub-n--grind.firebaseapp.com",
-  projectId: "grub-n--grind",
-  storageBucket: "grub-n--grind.appspot.com",
-  messagingSenderId: "391648176781",
-  appId: "1:391648176781:web:46a99023824f01bdae10c4"
-};
+    apiKey: "AIzaSyAoNcr7QGFQ_iOXpY0MzFBsdOSP06cBc-0",
+    authDomain: "grub-n--grind.firebaseapp.com",
+    projectId: "grub-n--grind",
+    storageBucket: "grub-n--grind.firebasestorage.app",
+    messagingSenderId: "391648176781",
+    appId: "1:391648176781:web:7b37e7646b6dd796ae10c4"
+  };
 
 // Initialize Firebase App
 const app = initializeApp(firebaseConfig);
